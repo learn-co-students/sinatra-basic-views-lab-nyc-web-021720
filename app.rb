1f54a94981d9c1061693b2af27f5486a8d173caa
codenameuriel <u.e.rodriguez@gmail.com>
2020-03-09 16:02:24 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,20 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do 
+
+
+    erb :index
+  end
+
+  
 
 end
+
+# Step 1:
+# In app.rb, you'll want to create a controller action responds to a GET request and loads index.erb in the browser.
+
+# Step 2:
+# Start writing HTML in index.erb. Run the rspec tests to make sure you have the appropriate content on the page.
+
+# View Sinatra Basic Views Lab on Learn.co and start learning to code for free.
